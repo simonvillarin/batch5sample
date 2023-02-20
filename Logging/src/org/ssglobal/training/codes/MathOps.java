a129a89525a33de0f8e66018f731b4efb206b7d1
simonvillarin <simon.villarin@gmail.com>
2023-02-20 15:29:39 +0800

    Logging Project. Version 1.0.

---
+++ Logging/src/org/ssglobal/training/codes/MathOps.java
@@ -0,0 +1,79 @@
+package org.ssglobal.training.codes;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class MathOps {
+	
+	// Step 1: Create or build the private logger object
+	private Logger logger = Logger.getLogger(MathOps.class.getName());
+	
+	public MathOps() {
+		// Step 2: Set the level of the logging
+		logger.setLevel(Level.ALL);
+		
+		// Step 3: Create another Handler
+		try {
+			FileHandler fileHandler = new FileHandler("./src/config/math.log", true);
+			fileHandler.setLevel(Level.ALL);
+			fileHandler.setEncoding("UTF-8");
+			
+			// Step 4: Create Format
+			SimpleFormatter txtFormatter = new SimpleFormatter();
+			fileHandler.setFormatter(txtFormatter);
+			
+			logger.addHandler(fileHandler);
+		} catch (SecurityException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public MathOps(int x, int y) {
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public int add(int x, int y) {
+		logger.info("start of method div() with parameters x=%d, y=%d".formatted(x, y));
+		
+		logger.warning("using custom Date() object");
+		Date xmas = new Date(123, 1, 32);
+		System.out.println(xmas);
+		int sum = x + y;
+		logger.info("end of method div() with parameters x=%d, y=%d".formatted(x, y));
+		return sum;
+	}
+
+	public int div(int x, int y) throws ArithmeticException, Exception {
+		logger.info("start of method div() with parameters x=%d, y=%d".formatted(x, y));
+	
+		if (y == 0) {
+			logger.severe("division by zero error");
+			throw new ArithmeticException();
+		}
+		
+		logger.info("end of method div() with parameters x=%d, y=%d".formatted(x, y));
+		return x / y;
+	}
+
+	public int[] displayArray(int[] arrData) {
+		logger.info("start of method div() with parameters arrData=%s".formatted(Arrays.toString(arrData)));
+
+		try {
+			int len = arrData.length;
+			System.out.println("length of array: %d".formatted(len));
+			System.out.println(Arrays.toString(arrData));
+		} catch (NullPointerException e) {
+			logger.severe(e.getMessage());
+		}
+		logger.info("end of method div() with parameters arrData=%s".formatted(Arrays.toString(arrData)));
+		return arrData;
+	}
+}
